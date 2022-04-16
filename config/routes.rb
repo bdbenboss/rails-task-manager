0a77510626e64c7250da81281cb9bd40a29d612e
Benjamin Deconinck <bdbenboss@hotmail.com>
2022-04-16 16:34:40 +0200

    crud finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+  resources :tasks
+
+  # get 'tasks', to: 'tasks#index'
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+  # get 'tasks/new', to: 'tasks#new', as: 'new_task'
+  # post 'tasks', to: 'tasks#create'
+  # get 'tasks/:id/edit', to: 'tasks#edit'
+  # patch 'tasks/:id', to: 'tasks#update'
+  # delete 'tasks/:id', to: 'tasks#destroy'
+
+  # delete '/tasks/:id', to: 'tasks#destroy'
+  # get '/tasks', to: 'tasks#index'
+  # get '/tasks/new', to: 'tasks#new', as: 'new_task'
+  # get '/tasks/:id', to: 'tasks#show', as: 'task'
+  # post '/tasks', to: 'tasks#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
